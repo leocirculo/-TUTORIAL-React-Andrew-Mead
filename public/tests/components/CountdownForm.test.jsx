5a7f4cbf2f613bb9c7a9d82b05d9949ea6cb695b
leoggonzalez <leoggonzalez@hotmail.com>
2018-10-31 22:29:45 +0100

    added async tests

--- public/tests/components/CountdownForm.test.jsx
+++ public/tests/components/CountdownForm.test.jsx
@@ -7,16 +7,36 @@ import CountdownForm from './../../components/CountdownForm';
 
 Enzyme.configure({ adapter: new Adapter() });
 
+beforeEach(() => {
+  jest.spyOn(console, 'error')
+  console.error.mockImplementation(() => {})
+})
+
+afterEach(() => {
+  console.error.mockRestore()
+});
+
 describe('CountdownForm', () => {
   it('should call onSetCountdown if valid seconds entered', () => {
     const mockFn = jest.fn();
     const countdownForm = mount(<CountdownForm onSetCountdown={mockFn} />);
     const input = countdownForm.find('input');
 
-    input.instance().value = 109;
+    input.instance().value = '109';
 
     countdownForm.find('form').simulate('submit');
 
     expect(mockFn).toHaveBeenCalledWith(109);
+  });
+  it('it should not call onSetCountdown when invalid input (NaN)', () => {
+    const mockFn = jest.fn();
+    const countdownForm = mount(<CountdownForm onSetCountdown={mockFn} />);
+    const input = countdownForm.find('input');
+
+    input.instance().value = 'Testing';
+
+    countdownForm.find('form').simulate('submit');
+
+    expect(mockFn).not.toHaveBeenCalled();
   });
 });
--- public/views/Countdown.jsx
+++ public/views/Countdown.jsx
@@ -9,11 +9,19 @@ export default class Countdown extends React.Component {
     super(props);
     this.state = {
       count: 0,
+      countDownStatus: 'stopped',
     };
   };
   
-  handleSetCountdown(seconds) {
-    this.setState({ count: seconds })
+  componentDidUpdate(prevProps, prevState) {
+    const { countDownStatus } = this.state;
+    if (countDownStatus !== prevState.countDownStatus) {
+      switch(countDownStatus) {
+        case 'started':
+          this.startTimer();
+          break;
+      }
+    }
   };
 
   render() {
@@ -25,6 +33,22 @@ export default class Countdown extends React.Component {
           <CountdownFrom onSetCountdown={this.handleSetCountdown.bind(this)}></CountdownFrom>
         </div>
       </Layout>
-    )
+    );
+  };
+
+  handleSetCountdown(seconds) {
+    this.setState({
+      count: seconds,
+      countDownStatus: 'started',
+    });
+  };
+
+  startTimer() {
+    this.timer = setInterval(() => {
+      const newCount = this.state.count - 1;
+      this.setState({ 
+        count:  newCount >= 0 ? newCount : 0,
+      });
+    }, 1000);
   }
 };
